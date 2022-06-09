1dce90dbf7d2c8de490a4c5a498ce0c637847e4a
DDinoBox <dinobox.create@gmil.com>
2022-06-09 18:53:14 +0900

    Social login test.py 구현중

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from django.test   import TestCase, Client
+
+from users.models  import User
+
+class KakaoSignInTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            kakao_id  = '1234567890',
+            nick_name = 'test',
+            email     = 'test@gmail.com'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+    
+class SignInTest(TestCase):
+    @patch('users.views.requests')
+    def test_kakao_signin_view_get_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            status_code = 200
+            
+            def json(self):
+                return {
+                    'id': '987654321',
+                    'properties': {
+                        'nickname': 'test'
+                    },
+                    'profile': {
+                        'email': 'test@gmail.com',
+                    }
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers = {"HTTP_Authorization" : "123123"}
+        response = client.get("/users/kakao", **headers)
+
+        self.assertEqual(response.status_code, 201 | 200) 
+            
+    @patch('users.views.requests')
+    def test_kakao_signin_fail_key_error(self, mocked_requests):
+        client = Client()
+        
+        class MockedResponse:
+            status_code = 400
+
+            def json(self):
+                return {
+                    'id' : 2145645622,
+                    'kakao_account' : {
+                        'profile' : {
+                            'nickname' : 'test'
+                        }
+                    }
+                }
+        
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'Authorization' : '123123'}
+        response            = client.get('/users/kakao', **headers)
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'Message' : 'Key error'})
--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ class KakaoSignInView(View):
         kakao_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
         redirect_uri = settings.KAKAO_REDIRECT_URI
         client_id = settings.KAKAO_CLIENT_ID
-        
+
         return redirect(f'{kakao_api}&client_id={client_id}&redirect_uri={redirect_uri}')
 
 class kakaoCallBackView(View):
@@ -23,16 +23,16 @@ class kakaoCallBackView(View):
             'client_id'      : settings.KAKAO_CLIENT_ID,
             'code'           : request.GET['code']
         }
-        
+
         kakao_token_api = 'https://kauth.kakao.com/oauth/token'
         access_token = requests.post(kakao_token_api, data=data).json()['access_token']
-                
+
         kakao_user_info_api = 'https://kapi.kakao.com/v2/user/me'
         user_info_response  = requests.get(kakao_user_info_api, headers={'Authorization' : f'Bearer {access_token}'}).json()
         kakao_id  = user_info_response['id']
         nick_name = user_info_response['properties']['nickname']
         email     = user_info_response['kakao_account']['email']
-        
+
         user, is_created = User.objects.get_or_create(
                 kakao_id = kakao_id,
                 defaults = {
@@ -40,13 +40,11 @@ class kakaoCallBackView(View):
                     'email'    : email
                 }
         )
-        
+
         data = {
             'access_token': jwt.encode({'id' : user.id}, settings.SECRET_KEY, settings.ALGORITHM),
             'nick_name'   : nick_name,
             'email'       : email
         }
-        
-        return JsonResponse({'Message' : 'SUCCESS', 'data' : data}, status = 200)
 
-        
+        return JsonResponse({'Message' : 'SUCCESS', 'data' : data}, status = 200)
