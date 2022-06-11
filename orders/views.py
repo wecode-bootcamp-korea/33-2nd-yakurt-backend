68937e2e1a2255de945443875ee022e0cefcde46
박상연 <psy@Sangyeon.local>
2022-06-11 15:52:28 +0900

    주문API 마이페이지 구현

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
 import json
 import uuid
 
-from django.views import View
-from django.http  import JsonResponse
-from django.db    import transaction
+from django.views         import View
+from django.http          import JsonResponse
+from django.db            import transaction
+from django.forms         import FloatField
+from django.db.models     import F, Sum, DecimalField
 
 from core.utils           import login_decorator
 from carts.models         import Cart
@@ -11,6 +13,7 @@ from subscriptions.models import Subscription
 from users.models         import User
 from orders.models        import Order,OrderItem,PaymentMethod
 from subscriptions.models import Subscription,SubscriptionItem
+from products.models      import Product
 
 class OrderView(View):
     @login_decorator
@@ -70,3 +73,27 @@ class OrderView(View):
             return JsonResponse({'message':'TransactionManagementError'}, status=400)  
         except KeyError:
             return JsonResponse({"message" : "KEYERROR"}, status=400)
+            
+    @login_decorator
+    def get(self, request):
+
+        orders = Order.objects.filter(user=request.user)
+
+        results = [{
+            'order_number': order.order_number,
+            'order_date'  : order.created_at,
+            'product'     : [{
+            'order_item': orderitem.product.name,
+            'img'       : orderitem.product.image_url,
+            'quantity'  : orderitem.quantity,
+            'price'     : orderitem.product.price,
+            } for orderitem in order.orderitem_set.all()],
+            'total_bill'      : order.orderitem_set.aggregate(item_price=Sum(F('product__price') * F('quantity') * 0.9 + 2500, output_field = DecimalField()))['item_price'],
+            'user_name'       : order.user.nick_name,
+            'user_address'    : order.user.address,
+            'user_phonenumber': order.user.phone_number,
+            'delivery_message': order.delivery_message,
+            'payment_method'  : order.payment_method.payment
+        } for order in orders]
+
+        return JsonResponse({'results': results}, status=200) 
