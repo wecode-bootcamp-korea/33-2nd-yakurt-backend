5c59e9cc744446dc60a4ad13ba88dcb4fde31d20
DDinoBox <dinobox.create@gmil.com>
2022-06-11 15:19:41 +0900

    Survey get 구현
    survey get 구현

--- survey/views.py
+++ survey/views.py
@@ -54,3 +54,58 @@ class UserSurveyView(View):
         }for user_survey in user_survey]
         
         return JsonResponse({'results': results}, status=200)
+class UserSurveyDetailView(View):
+    @login_decorator
+    def get(self, request):
+        user = request.user
+        
+        user_survey = UserSurvey.objects.filter(user_id = user.id).order_by('created_at').last()
+        
+        results = [{
+            'user_survey_id': user_survey.id,
+            'user'          : user.nick_name,
+            'gender'        : user_survey.gender,
+            'age'           : user_survey.age,
+            'BMI'           : float(round((((user_survey.weight) / (user_survey.height**2))*10000),1)),
+            'survey_product': [{
+                'product_id'         : surveyproduct.product.id,
+                'product_name'       : surveyproduct.product.name,
+                'product_information': surveyproduct.product.information,
+                'product_image_url'  : surveyproduct.product.image_url,
+                'product_description': surveyproduct.product.description,
+                'product_effect'     : [effect.name for effect in surveyproduct.product.effect_set.all()],    
+            }for surveyproduct in user_survey.surveyproduct_set.all()]
+        }]
+        
+        return JsonResponse({'results': results}, status=200)
+
+class UserSurveyListDetailView(View):
+    @login_decorator
+    def get(self, request,user_survey_id):
+        try : 
+            user = request.user
+            
+            user_survey = UserSurvey.objects.get(id = user_survey_id)
+            
+            results = [{
+                'user_survey_id': user_survey.id,
+                'user'          : user.nick_name,
+                'gender'        : user_survey.gender,
+                'age'           : user_survey.age,
+                'BMI'           : float(round((((user_survey.weight) / (user_survey.height**2))*10000),1)),
+                'survey_product': [{
+                    'product_id'         : surveyproduct.product.id,
+                    'product_name'       : surveyproduct.product.name,
+                    'product_information': surveyproduct.product.information,
+                    'product_image_url'  : surveyproduct.product.image_url,
+                    'product_description': surveyproduct.product.description,
+                    'product_effect'     : [effect.name for effect in surveyproduct.product.effect_set.all()],    
+                }for surveyproduct in user_survey.surveyproduct_set.all()]
+            }]
+            return JsonResponse({'results': results}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'Message': 'KEY_ERROR'}, status=400)
+        
+        except UserSurvey.DoesNotExist:
+            return JsonResponse({'Message': 'USERSURVEY_DOES_NOT_EXIST'}, status=404)
