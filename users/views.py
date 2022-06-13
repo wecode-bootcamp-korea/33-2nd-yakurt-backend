54b05be4fd8c7f3f2dd5233531ae0e1579ce60d9
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-06-13 11:06:45 +0900

    mentor review

--- users/views.py
+++ users/views.py
@@ -1,50 +1,94 @@
-import json, requests, jwt
+import json
 
+import requests
+import jwt
 from django.shortcuts import redirect
-from django.http  import JsonResponse
-from django.views import View
-from django.conf  import settings
+from django.http      import JsonResponse
+from django.views     import View
 
+from yakurt.settings import (
+    KAKAO_CLIENT_ID, 
+    KAKAO_REDIRECT_URI, 
+    SECRET_KEY, 
+    ALGORITHM
+)
 from users.models import User
+
+class KaKaoClient:
+    def __init__(self, kakao_client_id):
+        self.kakao_code_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
+        self.kakao_auth_api = 'https://kauth.kakao.com/oauth/token'
+        self.kakao_user_api = 'https://kapi.kakao.com/v2/user/me'
+        self.client_id      = kakao_client_id
+
+    def get_auth_code(self, redirect_uri):
+        return f'{self.kakao_code_api}&client_id={self.client_id }&redirect_uri={redirect_uri}'
+
+    def get_access_token(self, code, redirect_uri):
+        data = {
+            'grant_type'     : 'authorization_code',
+            'redirection_uri': redirect_uri,
+            'client_id'      : self.client_id,
+            'code'           : code
+        }
+
+        response = requests.post(self.kakao_auth_api, data=data, timeout=5)
+        
+        if response.status_code == 400:
+            raise Exception('AUTH_CODE_INVALID_ERROR' , 400)
+        
+        return response.json()['access_token']
+    
+    def get_user_information(self, access_token):
+        try:
+            response  = requests.get(
+                self.kakao_user_api, 
+                headers = {'Authorization' : f'Bearer {access_token}'}, 
+                timeout = 5
+            )
+            
+            if response.status_code == 401:
+                raise Exception('ACCESS_TOKEN_INVALID_ERROR' , 401)
+        
+            return response.json()
+        except:
+           raise Exception('ACCESS_TOKEN_INVALID_ERROR' , 401)
 
 class KakaoSignInView(View):
     def get(self, request):
-        kakao_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
-        redirect_uri = settings.KAKAO_REDIRECT_URI
-        client_id = settings.KAKAO_CLIENT_ID
-
-        return redirect(f'{kakao_api}&client_id={client_id}&redirect_uri={redirect_uri}')
+        kakao_clinet = KaKaoClient(KAKAO_CLIENT_ID)
+        redirect_url = kakao_clinet.get_auth_code(KAKAO_REDIRECT_URI)
+        return redirect(redirect_url)
 
 class kakaoCallBackView(View):
     def get(self,request):
-        data = {
-            'grant_type'     : 'authorization_code',
-            'redirection_uri': settings.KAKAO_REDIRECT_URI,
-            'client_id'      : settings.KAKAO_CLIENT_ID,
-            'code'           : request.GET['code']
-        }
+        try:
+            auth_code    = request.GET['code']
+            kakao_clinet = KaKaoClient(KAKAO_CLIENT_ID)
 
-        kakao_token_api = 'https://kauth.kakao.com/oauth/token'
-        access_token = requests.post(kakao_token_api, data=data).json()['access_token']
+            access_token     = kakao_clinet.get_access_token(KAKAO_REDIRECT_URI)
+            user_information = kakao_clinet.get_user_information(auth_code, access_token)
 
-        kakao_user_info_api = 'https://kapi.kakao.com/v2/user/me'
-        user_info_response  = requests.get(kakao_user_info_api, headers={'Authorization' : f'Bearer {access_token}'}).json()
-        kakao_id  = user_info_response['id']
-        nick_name = user_info_response['properties']['nickname']
-        email     = user_info_response['kakao_account']['email']
+            kakao_id  = user_information['id']
+            nick_name = user_information['properties']['nickname']
+            email     = user_information['kakao_account']['email']
 
-        user, is_created = User.objects.get_or_create(
-                kakao_id = kakao_id,
-                defaults = {
-                    'nick_name': nick_name,
-                    'email'    : email
-                }
-        )
+            user, is_created = User.objects.get_or_create(
+                    kakao_id = kakao_id,
+                    defaults = {
+                        'nick_name': nick_name,
+                        'email'    : email
+                    }
+            )
+            
+            data = {
+                'access_token': jwt.encode({'id' : user.id}, SECRET_KEY, ALGORITHM),
+                'nick_name'   : nick_name,
+                'email'       : email
+            }
 
-        data = {
-            'access_token': jwt.encode({'id' : user.id}, settings.SECRET_KEY, settings.ALGORITHM),
-            'nick_name'   : nick_name,
-            'email'       : email
-        }
-
-        return JsonResponse({'Message' : 'SUCCESS', 'data' : data}, status = 200)
+            return JsonResponse({'Message' : 'SUCCESS', 'data' : data}, status = 200)
+        except KeyError:
+            return JsonResponse({'Message' : 'KEY ERROR'}, status = 400)
+        except Exception as e:
+            return JsonResponse({'message' : e.args[0]} , status = e.args[1])
